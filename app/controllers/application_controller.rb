c5107706a521242bb2897fa1654b6161de624cae
Santiago Acosta <sacostal@chicagobooth.edu>
2023-04-10 20:45:23 +0000

    Square/new works 100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+
+    render({ :template => "calc_templates/square_form.html.erb"})
+
+  end
+
+  def square_results 
+
+    @num = params.fetch("number").to_f
+    @square_of_num = @num ** 2
+
+    render({ :template => "calc_templates/square_results.html.erb"})
+
+  end
+
 end
